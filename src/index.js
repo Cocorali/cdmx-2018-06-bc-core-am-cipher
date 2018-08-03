d33a1f00e43f80a6e9c1779ab48402a1c4f31f15
[Copelia] <iamcorallium@gmail.com>
2018-08-02 19:54:57 -0500

    Aplicando un poco de clean code para nombrar variables y eliminar elementos para sintetizar DOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //Aquí va todo lo del DOM: todo lo que mandamos y recibimos del html
 
 // Tomar input desde html
-    const messageCode = document.getElementById('mensajito');
-    const messageDecode = document.getElementById('mensajito2');
+    let messageCode = document.getElementById('message-to-code');
+    let messageDecode = document.getElementById('message-to-decode');
     const offsetJs = document.getElementById('offset');
 
 //Función arrow para cifrar
@@ -10,7 +10,7 @@ const buttonCode = () => {
   let message = messageCode.value;
   let offset = parseInt(offsetJs.value);
 //Imprimiendo en HTML mi resultado. cipher es objeto, encode es método, entre paréntesis mis argumentos.
-document.getElementById('cajitaCifrado').innerHTML = cipher.encode(message,offset);
+document.getElementById('message-to-decode').innerHTML = cipher.encode(message,offset);
 }
 
 //Función arrow para descifrar
@@ -18,5 +18,5 @@ const buttonDecode = () => {
   let message2 = messageDecode.value;
   let offset = parseInt(offsetJs.value);
 
-document.getElementById('cajitaDescrifado').innerHTML = cipher.decode(message2, offset);
+document.getElementById('message-to-code').innerHTML = cipher.decode(message2, offset);
 }
